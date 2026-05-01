d0996b7e49e952a46d80b32fed09b00eda324102
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-05-01 19:17:50 +0200

    feat: implement random color generator and history

--- lib/main.dart
+++ lib/main.dart
@@ -3,120 +3,147 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+const bool _debugMode = false;
+///Root widget of the application
 class MyApp extends StatelessWidget {
+  ///Creates MyApp instance
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(title: 'Random color generator'),
+      debugShowCheckedModeBanner: _debugMode,
     );
   }
 }
-
+///Main page of the color generating application
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  ///Creates MyHomePage instance
+  const MyHomePage({required this.title, super.key});
+  ///The title of the application
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Color _backgroundColor = Colors.blue;
+  final int _numberOfCharacters = 6;
+  final List<Color> _colorHistory = [];
+  final List<String> _availableCharacters = 
+  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+   'A', 'B', 'C', 'D', 'E', 'F' ];
+  
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          GestureDetector(
+            onTap: _changeColor,
+            behavior: HitTestBehavior.opaque,
+            child: Container(
+              color: _backgroundColor,
+              alignment: Alignment.center,
+              child: Text(
+                'Hello there!', 
+                style: TextStyle(
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold,
+                  color: _getSecondaryColor(),
+                ),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Align(
+            alignment: AlignmentGeometry.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _colorHistory.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _buildHistoryBox(_colorHistory[index]);
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
+      )
+    );
+  }
+
+  void _changeColor(){
+    setState(() {
+      final List<String> tempCharactersList = _availableCharacters;
+      const String rgbString = '0xFF';
+      final int randomIndex = DateTime.now()
+        .microsecondsSinceEpoch % tempCharactersList.length;
+
+      final StringBuffer buffer = StringBuffer(rgbString);
+
+      for (var i = 0; i < _numberOfCharacters; i++) {
+        tempCharactersList.shuffle();
+        buffer.write(tempCharactersList[randomIndex]);
+      }
+
+      _backgroundColor = Color(int.parse(buffer.toString()));
+      _colorHistory.add(_backgroundColor);
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (_scrollController.hasClients)
+        {
+          _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent, 
+            duration: const Duration(milliseconds: 300), 
+            curve: Curves.easeInOut
+            );
+        }
+      });
+
+      if (!_debugMode){
+        return;
+      }
+
+      for (var i = 0; i < _colorHistory.length; i++) {
+        debugPrint(_colorHistory[i].toARGB32().toString());
+      }
+    });
+  }
+
+  Widget _buildHistoryBox(Color color){
+    return Container(
+      width: 70,
+      margin: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(color: _getSecondaryColor()),
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [BoxShadow(
+          color: Colors.black.withAlpha(150),
+          blurRadius: 4,
+          offset: const Offset(0, 2)
+        )]
       ),
     );
+  }
+
+  Color _getSecondaryColor(){
+    return _backgroundColor.computeLuminance() > 0.5 ?
+     Colors.black : Colors.white;
+  }
+
+  @override
+  void dispose()
+  {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,27 +4,31 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:random_color_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Verify initial text and background tap logic', 
+  (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that "Hello there!" is visible on start.
+    expect(find.text('Hello there!'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Find the main GestureDetectopr
+    final backgroundFinder = find.byType(GestureDetector).first;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Tap the background and re-render the frame
+    await tester.tap(backgroundFinder);
+    await tester.pumpAndSettle();
+
+    // Verify that "Hello there!" is still visible
+    expect(find.text('Hello there!'), findsOneWidget);
+
+    // Verify the history list exists
+    expect(find.byType(ListView), findsOneWidget);
   });
 }
